d5bdb00165edad53afe1960331c31ab23b03110a
Vitechka <maruga99@mail.ru>
2023-07-23 20:24:16 +0300

    практика гет-запрос, курс валют бб

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-#salam aleykum
+import http.client
+import json
+
+connection = http.client.HTTPSConnection("belarusbank.by")
+headers = {'Content-type': 'application/json'}
+connection.request("GET", "/api/kursExchange")
+response = connection.getresponse()
+response_body = response.read().decode('utf-8')
+#print("Status: {} and reason: {}".format(response.status, response.reason))
+#print(response.read())
+response_json = json.loads(response_body)
+print(response_json[0]["USD_in"])
+print(response_json[0]["USD_out"])
+
+print(response_json[0]["name"])
+connection.close()
+
